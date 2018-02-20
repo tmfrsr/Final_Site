e5903aeace7d5fdd16020cc6bcf3d523858ed925
Tom Fraser <thomfraser@gmail.com>
2018-02-20 18:10:14 +1100

    the basic files

---
+++ main.js
@@ -0,0 +1,16 @@
+// $(document).ready(function() {
+console.log("ammama");
+// })
+
+
+function setup() {
+createCanvas(windowWidth, windowHeight).parent("canvasbox")
+background(255, 0, 200)
+
+}
+
+
+function draw(){
+rect(20,20,50,50,)
+
+}
